a7d712e5461665b9eba4971fe4b6fb0cc95fd917
acple <silentsphere110@gmail.com>
2019-03-23 17:14:36 +0900

    update pack script

--- nuget/pack.bat
+++ nuget/pack.bat
@@ -1,5 +1,4 @@
+@echo
+cd /d %~dp0
 dotnet clean ../ChainingAssertion.Core.sln
-dotnet restore ../ChainingAssertion.Core.sln
-dotnet pack -c Release -o ../nuget ../ChainingAssertion.Core.MSTest/ChainingAssertion.Core.MSTest.csproj
-dotnet pack -c Release -o ../nuget ../ChainingAssertion.Core.NUnit/ChainingAssertion.Core.NUnit.csproj
-dotnet pack -c Release -o ../nuget ../ChainingAssertion.Core.Xunit/ChainingAssertion.Core.Xunit.csproj
+dotnet pack -c Release -o ../nuget ../ChainingAssertion.Core.sln
